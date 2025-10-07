cbd6232f7cf2f62e71cf070953f512b94e9687eb
kokonut <castaneda.jair.13@gmail.com>
2025-10-07 18:06:45 -0500

    feat: calculadora y creditos por Andy

--- src/IsilApplication.jsx
+++ src/IsilApplication.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { AppLayout } from "./ui/layouts/AppLayout";
 import { CalculadoraPage } from "./features/calculadora/CalculadoraPage";
+import { isDevelopment } from "./utils/isDevelopment";
 
 export const IsilApplication = () => {
+  const isDev = isDevelopment() ? '/' : '/landing-page';
   return (
     <>
-      <BrowserRouter basename="/landing-page">
+      <BrowserRouter basename={isDev}>
         <Routes>
           <Route path="/" element={<AppLayout />} />
           <Route path="/calculadora" element={<CalculadoraPage />} />
