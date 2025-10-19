6ded741fcc26e1953a6df012086943e9bc89b56a
Matt Castillo <mcastillo@colossusisfg.com>
2025-10-18 19:25:26 -0500

    "comprobando el webhook de discord"

--- src/features/recursos/RecursosPage.tsx
+++ src/features/recursos/RecursosPage.tsx
@@ -1,7 +1,7 @@
 import "./recursos.css";
 
 
-//PRUEBA DE CONEXION A DISCORD
+//PRUEBA DE CONEXION A DISCORD1
 const recursosIcons: { linkIcon: string; descripcion: string; linkRef: string }[] = [
     {
         descripcion: "Cursos Gratuitos",
